40ba5dbc467733db25c1b7765c1210eea5c0bc50
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-01-10 15:47:47 +0100

    refactor(routes): extract routes to own files

--- src/emailpro/emailpro.module.js
+++ src/emailpro/emailpro.module.js
@@ -10,40 +10,7 @@ angular
       'emailproConstants',
       'emailproControllers',
       'emailproFilters',
+      'emailproRoutes',
       'emailproServices',
     ],
-  )
-  .config(['$stateProvider', ($stateProvider) => {
-    $stateProvider.state('app.email-pro', {
-      url: '/configuration/email_pro/:productId?tab',
-      templateUrl: 'emailpro/emailpro.html',
-      controller: 'EmailProCtrl',
-      reloadOnSearch: false,
-      resolve: {
-        navigationInformations: ['Navigator', '$rootScope', (Navigator, $rootScope) => {
-          _.set($rootScope, 'currentSectionInformation', 'email_pro');
-          return Navigator.setNavigationInformation({
-            leftMenuVisible: true,
-            configurationSelected: true,
-          });
-        }],
-      },
-      translations: ['.'],
-    });
-    $stateProvider.state('app.email.mxplan', {
-      url: '/configuration/email_mxplan/:productId?tab',
-      templateUrl: 'emailpro/emailpro.html',
-      controller: 'EmailProCtrl',
-      reloadOnSearch: false,
-      resolve: {
-        navigationInformations: ['Navigator', '$rootScope', (Navigator, $rootScope) => {
-          _.set($rootScope, 'currentSectionInformation', 'email_mxplan');
-          return Navigator.setNavigationInformation({
-            leftMenuVisible: true,
-            configurationSelected: true,
-          });
-        }],
-      },
-      translations: ['.'],
-    });
-  }]);
+  );
