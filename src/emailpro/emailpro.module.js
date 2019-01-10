e90937065b392350ebb9af94db63a79f30a87fe0
FredericEspiau <frederic.espiau@corp.ovh.com>
2019-01-10 15:47:47 +0100

    refactor(routes): extract routes to own files

--- src/emailpro/emailpro.module.js
+++ src/emailpro/emailpro.module.js
@@ -1,45 +1,16 @@
 angular
-  .module('Module.emailpro', [
-    'ovh-utils-angular',
-    'ngRoute',
-    'ui.bootstrap',
-    'ngSanitize',
-    'ng.ckeditor',
-    'emailProConstants',
-    'emailProControllers',
-    'emailProFilters',
-    'emailProServices'])
-  .config(['$stateProvider', ($stateProvider) => {
-    $stateProvider.state('app.email-pro', {
-      url: '/configuration/email_pro/:productId?tab',
-      templateUrl: 'emailpro/emailpro.html',
-      controller: 'EmailProCtrl',
-      reloadOnSearch: false,
-      resolve: {
-        navigationInformations: ['Navigator', '$rootScope', (Navigator, $rootScope) => {
-          _.set($rootScope, 'currentSectionInformation', 'email_pro');
-          return Navigator.setNavigationInformation({
-            leftMenuVisible: true,
-            configurationSelected: true,
-          });
-        }],
-      },
-      translations: ['.'],
-    });
-    $stateProvider.state('app.email.mxplan', {
-      url: '/configuration/email_mxplan/:productId?tab',
-      templateUrl: 'emailpro/emailpro.html',
-      controller: 'EmailProCtrl',
-      reloadOnSearch: false,
-      resolve: {
-        navigationInformations: ['Navigator', '$rootScope', (Navigator, $rootScope) => {
-          _.set($rootScope, 'currentSectionInformation', 'email_mxplan');
-          return Navigator.setNavigationInformation({
-            leftMenuVisible: true,
-            configurationSelected: true,
-          });
-        }],
-      },
-      translations: ['.'],
-    });
-  }]);
+  .module(
+    'Module.emailpro',
+    [
+      'ovh-utils-angular',
+      'ngRoute',
+      'ui.bootstrap',
+      'ngSanitize',
+      'ng.ckeditor',
+      'emailProConstants',
+      'emailProControllers',
+      'emailProFilters',
+      'emailProRoutes',
+      'emailProServices',
+    ],
+  );
---
+++ src/emailpro/emailpro.routes.js
@@ -0,0 +1,37 @@
+angular
+  .module('emailProRoutes')
+  .config(($stateProvider) => {
+    $stateProvider.state('app.email-pro', {
+      url: '/configuration/email_pro/:productId?tab',
+      templateUrl: 'emailpro/emailpro.html',
+      controller: 'EmailProCtrl',
+      reloadOnSearch: false,
+      resolve: {
+        navigationInformations: ['Navigator', '$rootScope', (Navigator, $rootScope) => {
+          _.set($rootScope, 'currentSectionInformation', 'email_pro');
+          return Navigator.setNavigationInformation({
+            leftMenuVisible: true,
+            configurationSelected: true,
+          });
+        }],
+      },
+      translations: ['.'],
+    });
+
+    $stateProvider.state('app.email.mxplan', {
+      url: '/configuration/email_mxplan/:productId?tab',
+      templateUrl: 'emailpro/emailpro.html',
+      controller: 'EmailProCtrl',
+      reloadOnSearch: false,
+      resolve: {
+        navigationInformations: ['Navigator', '$rootScope', (Navigator, $rootScope) => {
+          _.set($rootScope, 'currentSectionInformation', 'email_mxplan');
+          return Navigator.setNavigationInformation({
+            leftMenuVisible: true,
+            configurationSelected: true,
+          });
+        }],
+      },
+      translations: ['.'],
+    });
+  });
